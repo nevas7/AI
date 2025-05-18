855b24731af77f911e3219c0c5065ace5e3338a0
Nevas <nevas.sridhar@gmail.com>
2025-05-18 12:00:07 -0500

    FilterDemo

--- Python Learning/Trail.py
+++ Python Learning/Trail.py
@@ -2,6 +2,8 @@ myList = [1, 2, 3, 4, 5]
 def multiplyBy2(item):
     return item * 2
 
+def filterOdd(item):
+    return item % 2 != 0
 
-
-print(list(map(multiplyBy2, myList)))
+# print(list(map(multiplyBy2, myList)))
+print(list(filter(filterOdd, myList)))
