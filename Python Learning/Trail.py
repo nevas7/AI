6e00264b9185c949d149e53d5a72352028b0a915
Nevas <nevas.sridhar@gmail.com>
2026-03-08 11:27:44 -0500

    PY-RecapInput

--- Python Learning/Trail.py
+++ Python Learning/Trail.py
@@ -1,23 +1,32 @@
-myList = [1, 2, 3, 4, 5]
-def multiplyBy2(item):
-    return item * 2
+# myList = [1, 2, 3, 4, 5]
+# def multiplyBy2(item):
+#     return item * 2
 
-def filterOdd(item):
-    return item % 2 != 0
+# def filterOdd(item):
+#     return item % 2 != 0
 
-# print(list(map(multiplyBy2, myList)))
-print(list(filter(filterOdd, myList)))
+# # print(list(map(multiplyBy2, myList)))
+# print(list(filter(filterOdd, myList)))
 
-print(2+4)
-print(2/6)
-print(type(2+4))
-print(type(2/6))
+# print(2+4)
+# print(2/6)
+# print(type(2+4))
+# print(type(2/6))
 
-print(2//6)
-print(5//2) 
-print(4//2)
+# print(2//6)
+# print(5//2) 
+# print(4//2)
 
-print(round(2/6, 2))
-print(abs(-5))
+# print(round(2/6, 2))
+# print(abs(-5))
 
-print(int('0b101', 2))
+# print(int('0b101', 2))
+# player1 = 'Trent'
+# player2 = 'Coduy'
+# player3 = 'Aaron Judge'
+# print(f'Opening lineup: {player1}, {player2}, {player3}')
+
+username = input('Enter your username: ')
+password = input('Enter your password: ')
+
+print(f'{username}, your password {len(password)*"*"} is {len(password)} characters long.')
